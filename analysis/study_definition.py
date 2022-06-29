50fe26dc6bc24995cd6b806c6d6c122b452911c3
Yinghui Wei <91749253+yinghuiwei@users.noreply.github.com>
2022-06-29 16:31:44 +0100

    initial template for pre-vax study def

--- analysis/study_definition.py
+++ analysis/study_definition.py
@@ -2,12 +2,12 @@ from cohortextractor import StudyDefinition, patients, codelist, codelist_from_c
 
 
 study = StudyDefinition(
-    default_expectations={
-        "date": {"earliest": "1900-01-01", "latest": "today"},
-        "rate": "uniform",
-        "incidence": 0.5,
-    },
-    population=patients.registered_with_one_practice_between(
-        "2019-02-01", "2020-02-01"
-    ),
+  default_expectations={
+    "date": {"earliest": "1900-01-01", "latest": "today"},
+    "rate": "uniform",
+    "incidence": 0.5,
+  },
+  population=patients.registered_with_one_practice_between(
+    "2019-02-01", "2020-02-01"
+  ),
 )
