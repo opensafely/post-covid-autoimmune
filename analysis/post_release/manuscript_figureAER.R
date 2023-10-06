c4439da83d8f23ff41612374690584bc7f92a339
Jose Ignacio Cuitun Coronado <61969236+josephignace@users.noreply.github.com>
2023-10-06 12:01:33 +0100

    Outcome names
    Added outcome names to figures and tables scripts.

--- analysis/post_release/manuscript_figureAER.R
+++ analysis/post_release/manuscript_figureAER.R
@@ -6,18 +6,28 @@ df <- read.csv("output/post_release/lifetables_compiled.csv")
 # Filter data ------------------------------------------------------------------
 print("Filter data")
 
-# Neurodegenerative outcomes
-#4
-# df <- df[df$outcome %in% c("alzheimer_disease", "any_dementia", "cognitive_impairment", "migraine", "multiple_sclerosis", "rem_sleep_disorder","unspecified_dementias"),]
+# Autoimmune outcomes
 
-# df <- df[df$outcome %in% c("alzheimer_disease"),]
-
-# df <- df[df$outcome %in% c(""other_dementias", "parkinson_disease", "restless_leg_syndrome", "vascular_dementia"),]
-
-# df <- df[df$outcome %in% c("motor_neurone_disease"),]
-
-# df <- df[df$outcome %in% c("alzheimer_disease", "any_dementia", "cognitive_impairment", "migraine", "motor_neurone_disease", "multiple_sclerosis",
-#                            "other_dementias", "parkinson_disease", "rem_sleep_disorder", "restless_leg_syndrome", "unspecified_dementias", "vascular_dementia"),]
+# df <- df[df$outcome %in% c(#group 1
+#                           "ra", "undiff_eia", "pa", "axial",
+#                            #group 2
+#                            "sle", "sjs", "sss", "im", "mctd", "as", 
+#                            #group 3
+#                            "psoriasis", "hs", 
+#                            #group 4
+#                            "ibd", "crohn", "uc", "celiac", 
+#                            #group 5
+#                            "addison", "grave", "hashimoto_thyroiditis", 
+#                            #group 6
+#                            "anca", "gca", "iga_vasculitis", "pmr", 
+#                            #group 7 
+#                            "immune_thromb", "pernicious_anaemia", "apa", "aha", 
+#                            #group 8 
+#                            "glb", "multiple_sclerosis", "myasthenia_gravis", "longit_myelitis", "cis", 
+#                            #grouped outcomes 
+#                            "grp1_ifa", "grp2_ctd", "grp3_isd", "grp4_agi_ibd", "grp5_atv", "grp6_trd", "grp7_htd", "grp8_ind", 
+#                            #composite
+#                            "composite_ai"),]
 
 # Format aer_age ---------------------------------------------------------------
 print("Format aer_age")
