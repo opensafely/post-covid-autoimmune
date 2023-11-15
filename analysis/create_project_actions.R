f1ca759218d4ef9a367d31452faedc555738ffd2
Jose Ignacio Cuitun Coronado <61969236+josephignace@users.noreply.github.com>
2023-11-15 13:29:10 +0000

    Table 2 updates
    Changed table 2 output column and file name.

--- analysis/create_project_actions.R
+++ analysis/create_project_actions.R
@@ -464,6 +464,17 @@ actions_list <- splice(
     )
   ),
   
+  # action(
+  #   name = "make_other_output",
+  #   run = "r:latest analysis/model/make_table2_output.R",
+  #   needs = list("table2_prevax",
+  #                "table2_vax",
+  #                "table2_unvax"),
+  #   moderately_sensitive = list(
+  #     table2_output_rounded = glue("output/table2_output_rounded.csv")
+  #   )
+  # ),
+  
   ## venn output ------------------------------------------------------------
   
   # action(
