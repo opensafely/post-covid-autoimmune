6b1bee8b7bb644ba8c69d22f7d646f7157efbfec
Jose Ignacio Cuitun Coronado <61969236+josephignace@users.noreply.github.com>
2024-02-07 14:50:55 +0000

    Reduce covariates
    Filter cov_bin_history_ variables and merge

--- analysis/create_project_actions.R
+++ analysis/create_project_actions.R
@@ -134,8 +134,8 @@ join_study_definitions_data <- function(cohort){
     comment(glue("Join study definitions - {cohort}")),
   action(
     name = glue("join_study_definitions_{cohort}"),
-    run = glue("r:latest analysis/preprocess/join_study_definitions.R"),
-    #run = glue("r:latest analysis/preprocess/history_patients.R"),
+    #run = glue("r:latest analysis/preprocess/join_study_definitions.R"),
+    run = glue("r:latest analysis/preprocess/history_patients.R"),
     arguments = c(cohort),
     needs = list(glue("generate_study_population_{cohort}"), glue("generate_study_population_history_{cohort}")),
     #needs = list(glue("stage1_data_cleaning_{cohort}"), glue("generate_study_population_history_{cohort}")),
--- analysis/preprocess/join_study_definitions.R
+++ analysis/preprocess/join_study_definitions.R
@@ -27,12 +27,16 @@ print("Load history outcomes data set")
 
 input_history <- read_csv(paste0("output/input_",cohort,"_history.csv.gz"))
 
+# input_history <- input_history %>%
+#   select(-c("index_date_cohort", "end_date_outcome", "end_date_exposure", "cov_cat_sex", "vax_date_covid_1", 
+#             "tmp_exp_date_covid19_confirmed_sgss", "tmp_exp_date_covid19_confirmed_snomed", "tmp_exp_date_covid19_confirmed_hes", "tmp_exp_date_covid19_confirmed_death",  
+#             "dereg_date", "sub_date_covid19_hospital", "vax_jcvi_age_1", "vax_jcvi_age_2", "vax_cat_jcvi_group", "vax_date_eligible", "has_follow_up_previous_6months", "has_died", 
+#             "registered_at_start", "tmp_sub_bin_covid19_confirmed_history_sgss", "tmp_sub_bin_covid19_confirmed_history_snomed", "tmp_sub_bin_covid19_confirmed_history_hes", 
+#             "exp_date_covid19_confirmed", "sub_bin_covid19_confirmed_history"))
+
 input_history <- input_history %>%
-  select(-c("index_date_cohort", "end_date_outcome", "end_date_exposure", "cov_cat_sex", "vax_date_covid_1", 
-            "tmp_exp_date_covid19_confirmed_sgss", "tmp_exp_date_covid19_confirmed_snomed", "tmp_exp_date_covid19_confirmed_hes", "tmp_exp_date_covid19_confirmed_death",  
-            "dereg_date", "sub_date_covid19_hospital", "vax_jcvi_age_1", "vax_jcvi_age_2", "vax_cat_jcvi_group", "vax_date_eligible", "has_follow_up_previous_6months", "has_died", 
-            "registered_at_start", "tmp_sub_bin_covid19_confirmed_history_sgss", "tmp_sub_bin_covid19_confirmed_history_snomed", "tmp_sub_bin_covid19_confirmed_history_hes", 
-            "exp_date_covid19_confirmed", "sub_bin_covid19_confirmed_history"))
+  select(matches("patient_id|^cov_bin_history_"))
+  
 
 # merge data sets --------------------------------------------------------------
 print("Merge data sets")
@@ -46,6 +50,8 @@ input <- inner_join(input_outcome, input_history,
 
 # merge(input_outcome, input_history, 
 #            by = c("patient_id"))
+
+rm(input_outcome, input_history)
 
 # Save -------------------------------------------------------------------------
 print("Save final data set")
