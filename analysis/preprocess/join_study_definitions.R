efd326c86f5c69f41f0748c892a3e155ee47cf28
Jose Ignacio Cuitun Coronado <61969236+josephignace@users.noreply.github.com>
2024-02-08 14:15:51 +0000

    Simplified steps
    Removed lines.

--- analysis/preprocess/join_study_definitions.R
+++ analysis/preprocess/join_study_definitions.R
@@ -27,38 +27,23 @@ print("Load history outcomes data set")
 
 input_history <- read_csv(paste0("output/input_",cohort,"_history.csv.gz"))
 
-# input_history <- input_history %>%
-#   select(-c("index_date_cohort", "end_date_outcome", "end_date_exposure", "cov_cat_sex", "vax_date_covid_1", 
-#             "tmp_exp_date_covid19_confirmed_sgss", "tmp_exp_date_covid19_confirmed_snomed", "tmp_exp_date_covid19_confirmed_hes", "tmp_exp_date_covid19_confirmed_death",  
-#             "dereg_date", "sub_date_covid19_hospital", "vax_jcvi_age_1", "vax_jcvi_age_2", "vax_cat_jcvi_group", "vax_date_eligible", "has_follow_up_previous_6months", "has_died", 
-#             "registered_at_start", "tmp_sub_bin_covid19_confirmed_history_sgss", "tmp_sub_bin_covid19_confirmed_history_snomed", "tmp_sub_bin_covid19_confirmed_history_hes", 
-#             "exp_date_covid19_confirmed", "sub_bin_covid19_confirmed_history"))
+# Keep columns of interest -----------------------------------------------------
 
 input_history <- input_history %>%
   select(matches("patient_id|^cov_bin_history_"))
   
-
 # merge data sets --------------------------------------------------------------
 print("Merge data sets")
 
-input <- inner_join(input_outcome, input_history, 
-               by = c("patient_id"))#, "index_date_cohort", "end_date_outcome", "end_date_exposure", "cov_cat_sex", "vax_date_covid_1", 
-                  # "tmp_exp_date_covid19_confirmed_sgss", "tmp_exp_date_covid19_confirmed_snomed", "tmp_exp_date_covid19_confirmed_hes", "tmp_exp_date_covid19_confirmed_death",
-                  # "dereg_date", "sub_date_covid19_hospital", "vax_jcvi_age_1", "vax_jcvi_age_2", "vax_cat_jcvi_group", "vax_date_eligible", "has_follow_up_previous_6months", "has_died",
-                  # "registered_at_start", "tmp_sub_bin_covid19_confirmed_history_sgss", "tmp_sub_bin_covid19_confirmed_history_snomed", "tmp_sub_bin_covid19_confirmed_history_hes",
-                  # "exp_date_covid19_confirmed", "sub_bin_covid19_confirmed_history")) #left_join
+input <- left_join(input_outcome, input_history, 
+               by = c("patient_id")) #left_join or inner_join
 
-# merge(input_outcome, input_history, 
-#            by = c("patient_id"))
+# Remove data frame from the environment ---------------------------------------
+print("Remove dataframes from the environment")
 
 rm(input_outcome, input_history)
 
 # Save -------------------------------------------------------------------------
 print("Save final data set")
 
-#write_rds(input, file = paste0("output/input_",cohort,"_final.rds"))#, compress = "gz")
 saveRDS(input, file = paste0("output/input_",cohort,"_final.rds"), compress = "gzip")
-#saveRDS
-#write_csv(input, paste0("output/input_",cohort,"_final.csv.gz"))
-
-#write_csv(s1_input,paste0("output/input_",cohort,"_stage1_hosp.csv.gz"))
