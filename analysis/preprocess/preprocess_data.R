5c8ee7d5f2938fd4d577a46c9919cfc614a5831b
José Ignacio Cuitún Coronado <joseignacio.cuituncoronado@bristol.ac.uk>
2024-02-23 15:30:13 +0000

    Update preprocess
    Removed repeated lines and remove objects after being saved (following OS documentation).

--- analysis/preprocess/preprocess_data.R
+++ analysis/preprocess/preprocess_data.R
@@ -68,9 +68,15 @@ message ("Cohort ",cohort_name, " description written successfully!")
 
 # Add death_date from prelim data ----------------------------------------------
 
-prelim_data <- read_csv("output/index_dates.csv.gz",col_types=cols(patient_id = "c",death_date="D")) %>%
-  select(patient_id,death_date)
-df <- df %>% inner_join(prelim_data,by="patient_id")
+# prelim_data <- read_csv("output/index_dates.csv.gz",col_types=cols(patient_id = "c",death_date="D")) %>%
+#   select(patient_id,death_date)
+# df <- df %>% inner_join(prelim_data,by="patient_id")
+
+prelim_data <- read_csv("output/index_dates.csv.gz") 
+prelim_data <- prelim_data[,c("patient_id","death_date","deregistration_date")] 
+prelim_data$patient_id <- as.character(prelim_data$patient_id) 
+prelim_data$death_date <- as.Date(prelim_data$death_date) 
+prelim_data$deregistration_date <- as.Date(prelim_data$deregistration_date) 
 
 message("Death date added!")
 message(paste0("After adding death N = ", nrow(df), " rows"))
@@ -168,13 +174,15 @@ df1 <- df%>% select(patient_id, starts_with("index_date_"),
 ) %>% 
   select(-matches("tmp_"))
 
-df1[,colnames(df)[grepl("tmp_",colnames(df))]] <- NULL
+#df1[,colnames(df)[grepl("tmp_",colnames(df))]] <- NULL
 
-prelim_data <- read_csv("output/index_dates.csv.gz") 
-prelim_data <- prelim_data[,c("patient_id","death_date","deregistration_date")] 
-prelim_data$patient_id <- as.character(prelim_data$patient_id) 
-prelim_data$death_date <- as.Date(prelim_data$death_date) 
-prelim_data$deregistration_date <- as.Date(prelim_data$deregistration_date) 
+# prelim_data <- read_csv("output/index_dates.csv.gz") 
+# prelim_data <- prelim_data[,c("patient_id","death_date","deregistration_date")] 
+# prelim_data$patient_id <- as.character(prelim_data$patient_id) 
+# prelim_data$death_date <- as.Date(prelim_data$death_date) 
+# prelim_data$deregistration_date <- as.Date(prelim_data$deregistration_date) 
+
+# Add death_date from prelim data ----------------------------------------------
 
 df1 <- df1 %>% inner_join(prelim_data,by="patient_id") 
 
@@ -191,10 +199,12 @@ message(paste0("Input data saved successfully with N = ", nrow(df1), " rows"))
 sink(paste0("output/not-for-review/describe_input_",cohort_name,"_stage0.txt"))
 print(Hmisc::describe(df1))
 sink()
+rm(df1, prelim_data)
 
 # Restrict columns and save Venn diagram input dataset -----------------------
 
 df2 <- df %>% select(starts_with(c("patient_id","tmp_out_date","out_date")))
+rm(df)
 
 # Describe venn --------------------------------------------------------------
 
@@ -205,6 +215,7 @@ sink()
 # SAVE
 
 saveRDS(df2, file = paste0("output/venn_",cohort_name,".rds"))
+rm(df2)
 
 message("Venn diagram data saved successfully")
 tictoc::toc() 
